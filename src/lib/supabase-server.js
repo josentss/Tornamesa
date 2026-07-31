d13e1be0ddc3b5c5b50ada08394c3c7ef71fb21a
Josents <125105885+josentss@users.noreply.github.com>
2026-07-31 16:50:06 -0400

    Arreglando conexion a spotify general

--- src/lib/supabase-server.js
+++ src/lib/supabase-server.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 export function createSupabaseServer() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY
+    process.env.SUPABASE_SERVICE_ROLE_KEY
   );
 }
